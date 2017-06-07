48a8f2ad428f7320d1cce5fcc64fd50ecca761b1
Yong Cho <yongjec@gmail.com>
2017-06-06 22:58:20 -0400

    Reorganize for clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
   template: `
@@ -18,8 +23,4 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
-}
-export class Hero {
-  id: number;
-  name: string;
 }
